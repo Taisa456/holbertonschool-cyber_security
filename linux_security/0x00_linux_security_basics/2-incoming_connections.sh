905e16412b561ac55dd3b5c1736b951e324bae97
Taisa456 <bakhysheva2004@gmail.com>
2025-04-06 18:37:25 +0000

    TCP

--- linux_security/0x00_linux_security_basics/2-incoming_connections.sh
+++ linux_security/0x00_linux_security_basics/2-incoming_connections.sh
@@ -1,14 +1,2 @@
 #!/bin/bash
-
-# Set default incoming rule to deny
-sudo ufw default deny incoming
-
-# Allow incoming connections on port 80 (TCP)
-sudo ufw allow 80/tcp
-
-# Enable the firewall
-sudo ufw enable
-
-# Print a message confirming rules are updated
-echo "Rules updated"
-
+sudo ufw allow 80/tcp && sudo ufw default deny incoming && sudo ufw enable && echo "Rules updated"
