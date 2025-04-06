14fe1e70b4618058a1637a05775b1f84a04e4431
Taisa456 <bakhysheva2004@gmail.com>
2025-04-06 19:32:33 +0000

    scan

--- linux_security/0x00_linux_security_basics/7-scan.sh
+++ linux_security/0x00_linux_security_basics/7-scan.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
-
-# Check if an argument is provided
-if [ -z "$1" ]; then
-  echo "Usage: $0 <subnetwork or domain>"
-  exit 1
-fi
-
-# Perform the nmap scan
-echo "Starting Nmap scan on $1..."
-nmap -sn "$1"
+nmap -sn "$1" || echo "Usage: $0 <subnetwork or domain>"
