bc34c2b4e18773d65072828ee41ea2ff4b35ad0c
Taisa456 <bakhysheva2004@gmail.com>
2025-04-09 16:16:39 +0000

    last changes

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash 
-whois "$1" > temp.txt | echo "Registrant Information , Admin Information , Tech Information";
+whois "$1" > temp.txt; echo "Registrant Information , Admin Information , Tech Information";
