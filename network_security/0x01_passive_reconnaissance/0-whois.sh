5ac4dad59314d0f090818f701ff2ed7290bd54b6
Taisa456 <bakhysheva2004@gmail.com>
2025-04-10 07:42:15 -0400

    New

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash 
-whois "$1" | grep -E "Registrant|Admin|Tech"| sed 's/: /,/g' > "$1".csv
+whois "$1" | grep -E "Registrant|Admin|Tech"| grep -v "If you wish to contact"| sed 's/: /,/g' > "$1".csv
